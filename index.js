b9ba3d85bab6f98d30d3257defca8c6a9d37e3d9
Courtney Hampton <jaye0888@gmail.com>
2023-02-15 19:24:38 +0100

    Working on some JS activated styling

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const aboutBtn = document.getElementById("aboutBtn");
 const skillsBtn = document.getElementById("skillsBtn");
 const homeBtn = document.getElementById("homeBtn");
 const projectsBtn = document.getElementById("projectsBtn");
+const resumeBtn = document.getElementById("experienceBtn");
+
+//getting navSlider
+const navSlide = document.getElementById("navBtnBox");
 
 //getting page sections
 const skillSlide = document.getElementById("leftSlide");
@@ -15,10 +19,12 @@ const navigateToSkills = (e) => {
   homeSlide.style.transform = "translateX(100dvw)";
   projectSlide.style.right = "-100dvw";
   if (skillSlide) {
+    navSlide.style.transform = "translateX(20px)";
     skillsBtn.classList.add("active");
     homeBtn.classList.remove("active");
     projectsBtn.classList.remove("active");
     aboutBtn.style.opacity = 100;
+    resumeBtn.style.opacity = 0;
   }
 };
 const navigateToHome = (e) => {
@@ -26,10 +32,12 @@ const navigateToHome = (e) => {
   homeSlide.style.transform = "translateX(0)";
   projectSlide.style.right = "-100dvw";
   if (homeSlide) {
+    navSlide.style.transform = "translateX(-30px)";
     homeBtn.classList.add("active");
     skillsBtn.classList.remove("active");
     projectsBtn.classList.remove("active");
     aboutBtn.style.opacity = 0;
+    resumeBtn.style.opacity = 0;
   }
 };
 const navigateToProjects = (e) => {
@@ -37,10 +45,12 @@ const navigateToProjects = (e) => {
   homeSlide.style.transform = "translateX(-100dvw)";
   projectSlide.style.right = 0;
   if (projectSlide) {
+    navSlide.style.transform = "translateX(-100px)";
     projectsBtn.classList.add("active");
     skillsBtn.classList.remove("active");
     homeBtn.classList.remove("active");
     aboutBtn.style.opacity = 0;
+    resumeBtn.style.opacity = 100;
   }
   console.log("projects");
 };
